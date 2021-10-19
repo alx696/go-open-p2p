7123cf969368db52fd83b5f10a485fdb87180d28
李小明 <alx696@gmail.com>
2021-10-19 10:49:56 +0800

    完善api注释

--- op/api.go
+++ op/api.go
@@ -3,18 +3,30 @@ package op
 import "encoding/json"
 
 // TextSend 文本发送
+//
+// id 节点标识
+//
+// text 文本内容
 func TextSend(id, text string) error {
 	return textSend(id, text)
 }
 
 // FileSend 文件发送
 //
+// id 节点标识
+//
+// filePath 文件绝对路径
+//
 // 发送进度通过 Callback.OnOpFileSendProgress 获取
 func FileSend(id, filePath string) error {
 	return fileSend(id, filePath)
 }
 
-// ConnStateCheckSet 设置需要检查连接状态的节点ID数组
+// ConnStateCheckSet 设置需要检查连接状态的节点标识数组
+//
+// 通常应该将所有联系人的标识都设置进来
+//
+// 连接状态通过 Callback.OnOpConnState 获取
 func ConnStateCheckSet(idArrayText string) error {
 	var array []string
 	e := json.Unmarshal([]byte(idArrayText), &array)
